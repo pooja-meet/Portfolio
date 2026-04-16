ebc9e3646c67ece33a4e674f9eb1f778f15c7e3a
Pooja Rawat <pm9708989@gmail.com>
2026-04-16 21:57:18 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ const cors = require('cors')
 app.use(express.json())
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://itsmahi.netlify.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
 }));
@@ -46,6 +46,7 @@ connectDb().then(() =>
         console.log(`server is running at http://localhost:${PORT}`)
     )
 )
+
 
 
 
