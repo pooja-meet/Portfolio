21679fb9b8eecf11130bd8b8e3e525218b47396a
Pooja Rawat <pm9708989@gmail.com>
2026-04-19 16:56:04 +0530

    Update Navbar.jsx

--- frontend/src/user/components/Navbar/Navbar.jsx
+++ frontend/src/user/components/Navbar/Navbar.jsx
@@ -3,6 +3,7 @@ import './navbar.css'
 import { useState } from 'react';
 export default function Navbar() {
     const [open, setOpen] = useState(false);
+    const closeMenu = () => setOpen(false);
     return (
         <>
             <nav className='navbar'>
@@ -11,11 +12,12 @@ export default function Navbar() {
                     {open ? "✖" : "☰"}
                 </div>
                 <div className={`list_Container ${open ? "active" : ""}`}>
-                    <ul className='list'><li><Link to="/">Home</Link></li>
-                        <li><Link to="/skill" >Skill</Link></li>
-                        <li><Link to="/service">Service</Link></li>
-                        <li><Link to="/project">Project</Link></li>
-                        <li><Link to="/contact">Contact</Link></li>
+                    <ul className='list'>
+                        <li><Link to="/" onClick={closeMenu}>Home</Link></li>
+                        <li><Link to="/skill" onClick={closeMenu}>Skill</Link></li>
+                        <li><Link to="/service" onClick={closeMenu}>Service</Link></li>
+                        <li><Link to="/project" onClick={closeMenu}>Project</Link></li>
+                        <li><Link to="/contact" onClick={closeMenu}>Contact</Link></li>
                     </ul>
                 </div>
             </nav>
