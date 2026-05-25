b8cd0cee6aacad8db9afb7ea4061025812b1bc45
Pooja Rawat <pm9708989@gmail.com>
2026-05-25 16:17:40 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import './App.css'
+import './app.css'
 // Layouts
 import UserLayout from './layouts/User/UserLayout'
 import AdminLayout from './layouts/Admin/AdminLayout'
