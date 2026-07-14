b941ade4d93dee79d12c0b6e8d0c861b93c39203
Pooja Rawat <pm9708989@gmail.com>
2026-07-14 12:04:45 +0530

    Remove signup route and keep signin functionality

--- backend/Routes/authRoute.js
+++ backend/Routes/authRoute.js
@@ -7,26 +7,6 @@ const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const SECRET_KEY = process.env.SECRET_KEY || "helloji";
 
-router.post('/signup', async (req, res) => {
-    const { username, password } = req.body;
-    try {
-        if (!username || !password) {
-            return res.status(400).json({ msg: "fill form..." })
-        }
-        const existingUser = await AdminUser.findOne({ username })
-        if (existingUser) {
-            return res.status(400).json({ msg: "this name is already used by some other user" })
-        }
-        const hashPassword = await bcrypt.hash(password, 10)
-        const user = await AdminUser.create({
-            username, password: hashPassword
-        })
-        res.status(201).json({ msg: "user registered successfully", user: { id: user._id, username: user.username } })
-    } catch (error) {
-        console.error("server error", error.message)
-        res.status(500).json({ error: "server error" })
-    }
-})
 router.post('/signin', async (req, res) => {
     const { username, password } = req.body;
     try {
