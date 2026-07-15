9ca1d1bfa8d3d11e108bd4b9a47116a95c37e2e4
Pooja Rawat <pm9708989@gmail.com>
2026-07-15 12:10:37 +0530

    Fix formatting issues in Navbar component

--- frontend/src/user/components/Navbar/Navbar.jsx
+++ frontend/src/user/components/Navbar/Navbar.jsx
@@ -17,7 +17,7 @@ export default function Navbar() {
                         <li><Link to="/skill" onClick={closeMenu}>Skill</Link></li>
                         <li><Link to="/service" onClick={closeMenu}>Service</Link></li>
                         <li><Link to="/project" onClick={closeMenu}>Project</Link></li>
-                        <li><Link className='user_highlight' to="/contact" onClick={closeMenu}>Get in Touch</Link></li>
+                        <li className='user_highlight'><Link to="/contact" onClick={closeMenu}>Get in Touch</Link></li>
                     </ul>
                 </div>
             </nav>
